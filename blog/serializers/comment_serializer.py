e2c8a75aff5ec5bf14eb9830f902c0aeb8dcb446
santosh bohara <108050563+Sant7611@users.noreply.github.com>
2026-09-20 22:27:40 +0545

    Expose comment author avatars

--- blog/serializers/comment_serializer.py
+++ blog/serializers/comment_serializer.py
@@ -6,21 +6,34 @@ from base.serializers import BaseModelSerializer
 class CommentListSerializer(BaseModelSerializer):
     reply_count = serializers.IntegerField(source='replies.count' ,read_only=True)
     author = serializers.StringRelatedField(read_only=True)
+    author_avatar = serializers.SerializerMethodField()
+
+    def get_author_avatar(self, obj):
+        profile = getattr(obj.author, 'profile', None)
+        avatar = getattr(profile, 'avatar', None)
+        return avatar.url if avatar else None
     class Meta(BaseModelSerializer.Meta):
         model = Comment
-        fields=('id',  'author', 'parent', 'content','created_at', 'reply_count')
+        fields=('id',  'author', 'author_avatar', 'parent', 'content','created_at', 'reply_count')
         read_only_fields = BaseModelSerializer.Meta.read_only_fields + ('author', 'reply_count')
 
 
 class CommentDetailSerializer(BaseModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
+    author_avatar = serializers.SerializerMethodField()
     replies = CommentListSerializer(many=True, read_only=True)
+
+    def get_author_avatar(self, obj):
+        profile = getattr(obj.author, 'profile', None)
+        avatar = getattr(profile, 'avatar', None)
+        return avatar.url if avatar else None
 
     class Meta(BaseModelSerializer.Meta):
         model = Comment
         fields = (
             'id',
             'author',
+            'author_avatar',
             'parent',
             'content',
             'created_at',
@@ -29,5 +42,5 @@ class CommentDetailSerializer(BaseModelSerializer):
 
         read_only_fields = (
             BaseModelSerializer.Meta.read_only_fields
-            + ('author', 'replies')
+            + ('author', 'author_avatar', 'replies')
         )
